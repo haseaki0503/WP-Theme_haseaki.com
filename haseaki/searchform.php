52af460d1b871e27712cb9d8039a3b3bd5270e84
AKIRA HASEGAWA <haseaki0503@gmail.com>
2014-11-03 23:24:16 +0900

    Revert "Add files"
    This reverts commit 26a0929c7fbc566e6c216efcba7f782bace50a21.

--- haseaki/searchform.php
+++
@@ -1,4 +0,0 @@
-				<form mothod="get" id="searchform" action="#">
-					<input type="search" name="s" id="s">
-					<input type="submit" value="検索">
-				</form>
--- haseaki/sidebar.php
+++
@@ -1,8 +0,0 @@
-<?php if ( is_active_sidebar( 'sidebar-1' ) ) :
-dynamic_sidebar( 'sidebar-1' );
-else: ?>
-	<h2>No Widget</h2>
-	<div class="category">
-		<p>ウィジットは設定されていません。</p>
-	</div><!-- /.widget -->
-<?php endif; ?>
